9ffee1c3dc9e22c3ce21d19bfeedea8ef8ccde03
SandaruwanWGMA <molinduachintha24@gmail.com>
2023-10-02 12:26:38 +0530

    created login form

--- src/screens/LoggingScreen.jsx
+++ src/screens/LoggingScreen.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import CustomInput from "../components/CustomInput";
+import MoraMaths from "../assets/Icons/MoraMaths";
 
 function LoggingScreen(props) {
   return (
-    <div style={{ backgroundColor: "#050511" }}>
-      <img
-        className="logging-page-img"
-        src={require("../assets/Copy of Mora Maths- Vertical logo - Black text.png")}
-      ></img>
-      <CustomInput></CustomInput>
+    <div className="flex">
+      <div className="bg-dark h-screen w-2/5 md:w-full">
+        <div>
+          <div className="flex items-center justify-center text-light">
+            <MoraMaths className={"md:w-28 md:h-28"}></MoraMaths>
+          </div>
+          <div className="text-center">
+            <h2 className="text-light md:text-2xl text-4xl font-bold">
+              Login to your account
+            </h2>
+            <h2 className="text-light text-2xl ">Let's get started</h2>
+          </div>
+        </div>
+      </div>
+      <div>
+        <img
+          src={require("../assets/Images/smiling-young-geometry-teacher.png")}
+          className="md:hidden"
+        ></img>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      "2xl": { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
+
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+
+      xs: { max: "479px" },
+      // => @media (max-width: 479px) { ... }
+    },
+    extend: {
+      colors: {
+        dark: "#050511",
+        light: "#f5f5f5",
+        primary: "#B63E96", // 240,86,199
+        primaryDark: "#58E6D9", // 80,230,217
+        red: "#FF0000",
+      },
+    },
   },
   plugins: [],
 };
