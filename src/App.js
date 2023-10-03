f5ea13a143c07d8575a88aa8ecb1318315d37be2
Amila29 <amilakasun2000@gmail.com>
2023-10-03 21:41:07 +0530

    changes made in dev branch

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-
-
-
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Dashboard from './screens/dashboard';
+import "./App.css";
+// import { BrowserRouter, Route, Routes, Router } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./screens/dashboard";
 import logo from "./logo.svg";
-
-
+import Navbar from "./components/navbar/navbar";
 import QuizPage from "./screens/QuizPage/quizpage";
 import Quiz from "./screens/Quiz/quiz";
-import Home from "./screens/home";
+//import Home from "./screens/home";
 // import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/quizpage" element={<QuizPage />} />
-        <Route path="/quiz/:topic/:subTopic" element={<Quiz />} />
-      </Routes>
-    </Router>
-
-   
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/quizpage" element={<QuizPage />} />
+          <Route path="/quiz/Algebra/Geometry" element={<Quiz />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
