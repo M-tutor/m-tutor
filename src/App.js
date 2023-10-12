ea6c9c9f6615a83ff6d31a90a9c1bb40e582daab
Amila29 <amilakasun2000@gmail.com>
2023-10-12 12:05:26 +0530

    merged signup

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./screens/dashboard";
 import logo from "./logo.svg";
-import Navbar from "./components/navbar/Navbar";
+import Navbar from "./components/navbar/navbar";
 import Quiz from "./screens/Quiz/quiz";
 import QuizPage from "./screens/QuizPage/quizpage";
 import LoggingScreen from "./screens/LoggingScreen";
+import SignUp from "./screens/sign_up_screen/signup_screen";
 // import Quiz from "./screens/QuizPage/quizpage";
 //import Home from "./screens/home";
 // import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -17,11 +18,13 @@ function App() {
   return (
     <div>
       <BrowserRouter>
-        <Navbar />
+        {window.location.href === '/login' && <Navbar />}
         <Routes>
           <Route path="/quizpage" element={<QuizPage />} />
           <Route path="/quiz/Algebra/Geometry" element={<Quiz />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<LoggingScreen />} />
           {/* <Route path="/" element={<LoggingScreen />} /> */}
         </Routes>
       </BrowserRouter>
