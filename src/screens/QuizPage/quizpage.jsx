0bf112a3722ece3b0c5e7a52bb5abb360c7dfcd8
Sahiru Wijesinghe <w.sahiru@gtngroup.com>
2024-01-28 22:10:39 +0530

    Refactor QuizPage component and add loading icon

--- src/screens/QuizPage/quizpage.jsx
+++ src/screens/QuizPage/quizpage.jsx
@@ -1,19 +1,20 @@
-import React ,{useState,useEffect}from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 // import Navbar from "../../components/navbar/navbar";
 import QuizSubject from "../../components/quizsubject/quizsubject";
 import "./quizpage.css";
-import {firestore} from "../../config/firebase";
+import { firestore } from "../../config/firebase";
 import { collection, getDocs } from "firebase/firestore";
 import Navbar from "../../components/navbar/navbar";
-
+import LoadingIcon from "../../components/loading/LoadingIcon";
 function QuizPage() {
   const navigate = useNavigate();
-  const [tabData, setTabData] = useState([])
-  
+  const [tabData, setTabData] = useState([]);
+  let [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchQuizData = async () => {
+      setLoading(true);
       try {
         const quizCollection = collection(firestore, "QUizDetails");
         const querySnapshot = await getDocs(quizCollection);
@@ -27,6 +28,7 @@ function QuizPage() {
       } catch (error) {
         console.error("Error fetching quiz data:", error);
       }
+      setLoading(false);
     };
 
     fetchQuizData();
@@ -35,14 +37,14 @@ function QuizPage() {
   const handleTopic = (item, tabTitle) => {
     console.log(item, tabTitle);
     // navigate(`/quiz/${item}/${tabTitle}`);
-    navigate('/quizpage');
+    navigate("/quizpage");
   };
   return (
     <>
       <div className="navbar">
         <Navbar />
       </div>
-
+     { loading && <LoadingIcon />}
       <div className="container">
         <div className="h1">Quizzes</div>
         {tabData.map((tab, index) => (
